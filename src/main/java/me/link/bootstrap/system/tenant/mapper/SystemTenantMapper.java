566707d6aa6a0eb546776b34b0be1a3bff36ee53
zhangxb <863232387@qq.com>
2026-03-24 04:02:32 +0800

    ``` refactor(tenant): 移除租户模块相关代码
    - 删除系统租户实体类 SystemTenant
    - 删除系统租户套餐实体类 SystemTenantPackage
    - 删除租户服务接口 ISystemTenantService
    - 删除租户套餐服务接口 ISystemTenantPackageService
    - 删除租户服务实现类 SystemTenantServiceImpl
    - 删除租户套餐服务实现类 SystemTenantPackageServiceImpl
    - 删除租户控制器 SystemTenantController
    - 删除租户套餐控制器 SystemTenantPackageController
    - 删除租户映射器 SystemTenantMapper
    - 删除租户套餐映射器 SystemTenantPackageMapper
    - 删除租户映射器XML文件
    - 删除租户套餐映射器XML文件
    - 删除租户分页查询请求对象 TenantPageReqVO
    ```

--- src/main/java/me/link/bootstrap/system/tenant/mapper/SystemTenantMapper.java
+++
@@ -1,9 +0,0 @@
-package me.link.bootstrap.system.tenant.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import me.link.bootstrap.system.tenant.entity.SystemTenant;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SystemTenantMapper extends BaseMapper<SystemTenant> {
-}
